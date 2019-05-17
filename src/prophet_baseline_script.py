786ae46d441e3197edd1c388bbf35cc265fd2e8a
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-05-16 20:24:14 -0700

    Create predictions folder if it doesn't exist

--- src/prophet_baseline_script.py
+++ src/prophet_baseline_script.py
@@ -2,6 +2,7 @@
 # load packages
 import pandas as pd
 import numpy as np
+import os
 from fbprophet import Prophet
 
 # read data
@@ -12,18 +13,18 @@ train = raw_data[(raw_data["SHIFT_DATE"]>"2012-12-31") & (raw_data["SHIFT_DATE"]
 val = raw_data[(raw_data["SHIFT_DATE"]>"2016-12-31") & (raw_data["SHIFT_DATE"]<"2018-01-01")]
 
 # using only a portion of the sites
-small_train = train[(train["SITE"]=="St Paul's Hospital") | 
-                    (train["SITE"]=="Mt St Joseph") | 
-                    (train["SITE"]=="Holy Family") | 
-                    (train["SITE"]=="SVH Langara") | 
-                    (train["SITE"]=="Brock Fahrni") | 
+small_train = train[(train["SITE"]=="St Paul's Hospital") |
+                    (train["SITE"]=="Mt St Joseph") |
+                    (train["SITE"]=="Holy Family") |
+                    (train["SITE"]=="SVH Langara") |
+                    (train["SITE"]=="Brock Fahrni") |
                     (train["SITE"]=="Youville Residence")]
 
-small_val = val[(val["SITE"]=="St Paul's Hospital") | 
-                (val["SITE"]=="Mt St Joseph") | 
-                (val["SITE"]=="Holy Family") | 
-                (val["SITE"]=="SVH Langara") | 
-                (val["SITE"]=="Brock Fahrni") | 
+small_val = val[(val["SITE"]=="St Paul's Hospital") |
+                (val["SITE"]=="Mt St Joseph") |
+                (val["SITE"]=="Holy Family") |
+                (val["SITE"]=="SVH Langara") |
+                (val["SITE"]=="Brock Fahrni") |
                 (val["SITE"]=="Youville Residence")]
 
 # create training dataframes
@@ -40,17 +41,17 @@ timeframe = pd.DataFrame(pd.date_range(start='2017-01-01', end='2017-12-31', fre
 # method for running prophet models
 def run_prophet(series, timeframe=timeframe):
     """
-    Runs the Prophet 
-    
+    Runs the Prophet
+
     Key arguments:
     --------------
     series -- (DataFrame) time series data
-    timeframe -- (DataFrame) a DataFrame with one column 
+    timeframe -- (DataFrame) a DataFrame with one column
                  consisting of predicted dates
 
-    Returns: 
+    Returns:
     --------------
-    Returns the forecast of the predictions 
+    Returns the forecast of the predictions
 
     """
     model = Prophet(yearly_seasonality=True, weekly_seasonality=True, daily_seasonality=False, interval_width=0.95)
@@ -78,12 +79,12 @@ for i in small_sites:
         except ValueError:
             pred_results[(i, j)] = None
             print("Fitting -", i, j, ": Failed")
-            
+
 # combine predictions and true results
 combined = {}
 for i in true_results:
     if pred_results[i] is not None:
-        combined[i] = pd.merge(true_results[i], pred_results[i], on="ds", how="outer")[["ds", "y", "yhat", 
+        combined[i] = pd.merge(true_results[i], pred_results[i], on="ds", how="outer")[["ds", "y", "yhat",
                                                                                         "yhat_lower", "yhat_upper"]]
 
 # convert to week and calculating errors weekly
@@ -92,17 +93,17 @@ for i in combined:
     # create week column
     combined[i]["week"] = combined[i]["ds"].dt.week
     combined[i]["ds"] = combined[i]["ds"]-pd.DateOffset(weekday=0, weeks=1)
-    
+
     # store y, yhat, yhat_lower, yhat_upper
     weekly_y = combined[i].groupby("ds").y.sum().reset_index()
     weekly_yhat = combined[i].groupby("ds").yhat.sum().reset_index()
     weekly_yhat_lower = combined[i].groupby("ds").yhat_lower.sum().reset_index()
     weekly_yhat_upper = combined[i].groupby("ds").yhat_upper.sum().reset_index()
-    
+
     # merge weekly results
-    weekly[i] = pd.concat([weekly_y, weekly_yhat["yhat"], 
-                           weekly_yhat_lower["yhat_lower"], 
-                           weekly_yhat_upper["yhat_upper"]], 
+    weekly[i] = pd.concat([weekly_y, weekly_yhat["yhat"],
+                           weekly_yhat_lower["yhat_lower"],
+                           weekly_yhat_upper["yhat_upper"]],
                           axis=1)
 
     # create columns "year", "site", "labor_agreement"
@@ -110,9 +111,13 @@ for i in combined:
     weekly[i]["week"] = weekly[i]["ds"].dt.weekofyear
     weekly[i]["site"] = np.repeat(i[0], length)
     weekly[i]["labor_agreement"] = np.repeat(i[1], length)
-    
+
 # export to "data/predictions/" directory
+predictions_path = "../data/predictions/"
+if not os.path.exists(predictions_path):
+    os.mkdir(predictions_path)
+
 total_data = pd.DataFrame()
 for i in weekly:
     total_data = pd.concat([total_data, weekly[i]], axis=0)
-total_data.to_csv("../data/predictions/predictions.csv")
+total_data.to_csv(predictions_path + "predictions.csv")
